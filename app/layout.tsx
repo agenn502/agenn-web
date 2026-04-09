2b08536db07d2665b707a2be04e099041b50e25f
agenn502 <agenn.502@gmail.com>
2026-04-09 08:18:24 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,23 @@
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "AGENN",
-  description:
-    "Academia Guatemalteca de Estudios Numismáticos y Notafílicos",
+  description: "Academia Guatemalteca de Estudios Numismáticos y Notafílicos",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="es">
       <body>
